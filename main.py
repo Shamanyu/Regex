9a9ae7dc9b2cef74397df59b4dc22f9e48259f67
shamanyu <shubhamshamanyu@gmail.com>
2018-03-04 12:45:52 +0530

    Study important Python function

--- main.py
+++ main.py
@@ -6,8 +6,26 @@ class Regex():
     print ("Init function of Regex")
 
   def match(self, string, pattern):
+
     p = re.compile('ab*')
-    print (p)
+    print (p.match("dabc"))
+    print (p.search("dabc"))
+    print (p.match("abc").group())
+    print (p.match("abc").start())
+
+    p = re.compile(r'\d+')
+    m = p.match(' string goes here')
+    if m:
+      print('Match found: ', m.group())
+    else:
+      print('No match')
+
+    p = re.compile(r'\d+')
+    m = p.findall('12 drummers drumming, 11 pipers piping, 10 lords a-leaping')
+    if m:
+      print('Match found: ', m)
+    else:
+      print('No match')
 
 if __name__ == '__main__':
   regex = Regex()
